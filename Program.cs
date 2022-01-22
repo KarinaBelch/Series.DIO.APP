2dd9b9cf9b919c23e3fd803bd1ef01dfe0f728cc
Karina+ <karina.catuna@hotmail.com>
2022-01-22 13:09:34 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using DIO.Series.Classes;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static SerieRepositorio repositorio = new SerieRepositorio();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarSeries();
+                        break;
+                    case "2":
+                        InserirSerie();
+                        break;
+                    case "3":
+                        AtualizarSerie();
+                        break;
+                    case "4":
+                        ExcluiSerie();
+                        break;
+                    case "5":
+                        VisualizarSerie();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            System.Console.WriteLine("Agradecemos por utilizar nossos servicos.");
+            System.Console.ReadLine();
+        }
+
+        private static void VisualizarSerie()
+        {
+            System.Console.WriteLine("Digite o id da serie: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            var serie = repositorio.RetornaPorId(indiceSerie);
+
+            System.Console.WriteLine(serie);
+        }
+
+        private static void ExcluiSerie()
+        {
+            System.Console.WriteLine("Digite o id da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            repositorio.Exclui(indiceSerie);
+        }
+
+        private static void AtualizarSerie()
+        {
+            System.Console.WriteLine("Digite o id da serie: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                System.Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+            }
+
+            System.Console.WriteLine("Digite o genero entre as opcoes acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o Titulo da Série: ");
+            string? entradaTitulo = Console.ReadLine();
+
+            System.Console.WriteLine("Digite o ano de inicio da Série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite a Descricao da Série: ");
+            string? entradaDescricao = Console.ReadLine();
+
+            Series atualizaSerie = new Series(id: indiceSerie,
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescricao);
+            
+            repositorio.Atualiza(indiceSerie, atualizaSerie);
+        }
+
+        private static void InserirSerie()
+        {
+            System.Console.WriteLine("Inserir nova série");
+
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                System.Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            System.Console.WriteLine("Digite o genero entre as opcoes acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o Titulo da Série: ");
+            string? entradaTitulo = Console.ReadLine();
+
+            System.Console.WriteLine("Digite o ano de inicio da Série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite a Descricao da Série: ");
+            string? entradaDescricao = Console.ReadLine();
+
+            Series novaserie = new Series(id: repositorio.ProximoId(),
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescricao);
+            repositorio.Insere(novaserie);
+        }
+
+        private static void ListarSeries()
+        {
+            System.Console.WriteLine("Listar séries");
+
+            var lista = repositorio.Lista();
+
+            if (lista.Count == 0)
+            {
+                System.Console.WriteLine("Nenhuma série cadastrada.");
+                return;
+            }
+
+            foreach (var serie in lista)
+            {
+                var excluido = serie.retornaExcluidoId();
+
+                System.Console.WriteLine("#ID {0}: - {1} {2}", serie.retornaId(), serie.retornaTitulo(), (excluido ? "*Excluido*" : ""));
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine("Bem vindo ao DIO Séries!");
+            System.Console.WriteLine("Informe a opcao desejada:");
+            
+            System.Console.WriteLine("1- Listar séries");
+            System.Console.WriteLine("2- Inserir nova série");
+            System.Console.WriteLine("3- Atualizar série");
+            System.Console.WriteLine("4- Excluir série");
+            System.Console.WriteLine("5- Visualizar série");
+            System.Console.WriteLine("C- Limpar Tela");
+            System.Console.WriteLine("X- Sair");
+            System.Console.WriteLine();
+
+            string? opcaoUsuario = Console.ReadLine().ToUpper();
+            System.Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
